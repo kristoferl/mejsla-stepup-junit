98ecfa9f14c84faf72ab4b7d6b70f1cd86c77cbe
Kristofer Löfgren <kristofer.lovgren@gmail.com>
2017-03-20 09:17:03 +0100

    Created maven repo

---
+++ src/main/java/se/mejsla/stepup/App.java
@@ -0,0 +1,13 @@
+package se.mejsla.stepup;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
